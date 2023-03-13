2b44309fbfd03f937631ff70e920ea8a773be690
Patrick Borowy <patrick@exordelabs.com>
2023-03-13 08:05:46 +0100

    chore: cleaning up commented lines

--- stims/perpetuate.py
+++ stims/perpetuate.py
@@ -5,9 +5,7 @@ from stims.autofill import autofill
 
 
 async def perpetuate(function: Callable, args: Any=[], kwargs: Any={}) -> Any:
-    # update = await function(*args, **kwargs)
     update = await autofill(function, args=args, kwargs=kwargs)
     if isinstance(update, dict):
         kwargs.update(update)
-    # kwargs.update(update)
     return update
