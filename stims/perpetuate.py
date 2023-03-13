433cd25c22f9ac186e84b43e9ae8fdbb3c141fe1
Patrick Borowy <patrick@exordelabs.com>
2023-03-13 08:57:42 +0100

    dev: on decorator

--- stims/perpetuate.py
+++ stims/perpetuate.py
@@ -3,9 +3,23 @@ from typing import Callable, Any
 
 from stims.autofill import autofill
 
+ONS = {}
+
+def on(variable_name: str, condition: Callable|None=None):
+    def decorator(function: Callable):
+        if variable_name not in ONS:
+            ONS[variable_name] = []
+        ONS[variable_name].append((condition, function))
+        return function
+    return decorator
 
 async def perpetuate(function: Callable, args: Any=[], kwargs: Any={}) -> Any:
     update = await autofill(function, args=args, kwargs=kwargs)
     if isinstance(update, dict):
         kwargs.update(update)
+        for key, value in update.items():
+            if key in ONS:
+                for condition, func in ONS[key]:
+                    if (condition and await autofill(condition, args=[value], kwargs=kwargs)) or not condition:
+                        await autofill(func, args=args, kwargs=kwargs)
     return update
--- tests/test_perpetuate.py
+++ tests/test_perpetuate.py
@@ -1,6 +1,6 @@
 import pytest
 
-from stims.perpetuate import perpetuate
+from stims.perpetuate import perpetuate, on
 
 @pytest.mark.asyncio
 async def test_perpetuate():
@@ -16,3 +16,11 @@ async def test_perpetuate():
     assert await perpetuate(test_function2, kwargs=mem) == { 'result': 2 }
 
     assert mem['result'] == 2
+
+@pytest.mark.asyncio
+async def test_on(mocker):
+    mem = { 'a': 1 }
+    mock = mocker.Mock()
+    on('a')(mock)
+    assert await perpetuate(lambda: { 'a': 2 }, kwargs=mem)
+    mock.assert_called_once()
