a7f7c11757cf0dd2c7c769643d698654426391ce
Patrick Borowy <patrick@exordelabs.com>
2023-03-25 02:50:46 +0100

    chore(http): remove test_http while it's vaulted

--- tests/test_http.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from aiosow.http import aiohttp_server
-
-@pytest.mark.asyncio
-async def test_app_factory():
-    ''''''
-    # task = aiohttp_server()
-    # assert asyncio.iscoroutine(task)
