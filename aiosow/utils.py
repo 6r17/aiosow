02407a7b3b2c2a1bdfe97108446b5e0b7af8d8b6
Patrick Borowy <patrick@exordelabs.com>
2023-03-25 12:14:26 +0100

    chore(utils): delete unused utils.py

--- aiosow/utils.py
+++
@@ -1,9 +0,0 @@
-from functools import reduce
-from typing import Any
-
-def get_value_by_path(data, path, separator="__") -> Any:
-    keys = path.split(separator)
-    try:
-        return reduce(lambda d, key: d[int(key)] if isinstance(d, list) else d[key], keys, data)
-    except (KeyError, IndexError, TypeError):
-        return None
