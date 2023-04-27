f190c1561e3a1363661429d9ca17f1064604f0e8
Patrick Borowy <patrick@exordelabs.com>
2023-04-27 11:01:01 +0200

    test: routine test immediate fix

--- tests/test_routines.py
+++ tests/test_routines.py
@@ -8,28 +8,16 @@ def do_clear_routines():
 
 
 def test_routine_decorator():
-    @routine(interval=5, repeat=False, perpetuate=False)
+    @routine(5, perpetuate=False)
     def my_function():
         print("Hello world!")
 
     assert len(get_routines()) == 1
-    assert get_routines()[0]["interval"] == 5
     assert get_routines()[0]["function"] == my_function
-    assert get_routines()[0]["repeat"] == False
-    assert get_routines()[0]["life"] == 0
+    assert get_routines()[0]["timeout"] == 5
     assert get_routines()[0]["perpetuate"] == False
 
 
 @pytest.mark.asyncio
 async def test_consume_routines_empty():
     await consume_routines(memory={})
-
-
-@pytest.mark.asyncio
-async def test_consume_routines_with_a_routine():
-    routine(interval=1, life=1, repeat=False, perpetuate=True)(lambda a: a)
-    routine(interval=1, life=1, repeat=False, perpetuate=False)(lambda a: a)
-    routine(interval=1, life=0, repeat=True, perpetuate=False)(lambda a: a)
-    routine(interval=1, life=1, repeat=True, perpetuate=False)(lambda a: a)
-    await consume_routines(memory={})
-    assert get_routines()[1]["life"] == 1
