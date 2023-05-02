3aafe7fff66e0effe72d90bbbabc18dd4646cd90
Patrick Borowy <patrick@exordelabs.com>
2023-05-02 13:13:14 +0200

    chore: add description and long description to setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,30 @@
 from setuptools import setup, find_packages
 
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
-    name='aiosow',
-    version='0.0.1',
-    packages=find_packages(include=['aiosow']),
-    license='MIT',
+    name="aiosow",
+    version="0.1.2",
+    packages=find_packages(include=["aiosow"]),
+    description="An event-based framework",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
     entry_points={
-        'console_scripts': [
-            'aiosow = aiosow.command:run',
+        "console_scripts": [
+            "aiosow = aiosow.command:run",
         ],
     },
     install_requires=[],
     extras_require={
-        "aiohttp":[
-            'aiohttp',
-        ],
         "dev": [
             "pytest",
             "pytest-mock",
             "pytest-asyncio",
             "pytest-coverage",
         ],
-        "doc": [
-            "pdoc"
-        ]
-    }
+        "doc": ["pdoc"],
+    },
 )
