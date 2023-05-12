90fdf797a05b0611a334173e48b6836fc3305596
Patrick Borowy <112330025+6r17@users.noreply.github.com>
2023-05-12 12:55:07 +0200

    Bump version for new release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="aiosow",
-    version="0.1.4",
+    version="0.1.5",
     packages=find_packages(include=["aiosow"]),
     description="An event-based framework",
     long_description=long_description,
